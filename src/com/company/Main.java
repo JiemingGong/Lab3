b5d228c48dc3c2f1a86e5a33d59812404b2042c6
jieming <jieming2@illinois.edu>
2018-02-07 16:32:10 -0600

    count 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
-
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        contents = contents.toUpperCase();
+        contents = contents.replaceAll("[^a-zA-Z ]", "");
+        String[] result = contents.split(" ");
+        int count = 0;
+        for(int i = 0; i <= result.length - 1; i++) {
+            if (result[i].equals("PRINCE")){
+                count++;
+            };
+        }
+        System.out.println(count);
+        return contents;
+    }
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
     }
 }
